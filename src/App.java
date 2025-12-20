f32a66656d0e31917a7f9a7374013a115da29da4
SuhasNag12 <suhas.nag@perficient.com>
2025-12-20 22:16:54 +0530

    ClassObject Demo Program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Tommy", "PitBull", 3, "Brown");
+        System.out.println(dog1);
     }
 }
