80807d0f2b64e5633d04986bd01aaf591dffb152
SuhasNag12 <suhas.nag@perficient.com>
2025-12-21 18:37:48 +0530

    Program to demo Protected Access Modifier.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.model = "Ritz";
-        System.out.println("My Car's model is " + car.model);
+        Bike bike = new Bike();
+        bike.setSpeed(65);
+        System.out.println("I ride my bike with a speed of " + bike.getSpeed());
     }
 }
---
+++ src/Bike.java
@@ -0,0 +1,8 @@
+public class Bike extends Vehicle{
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+}
