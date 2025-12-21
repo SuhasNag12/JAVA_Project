84eff44a1682a3eccdfcc69d4fcf396e1d05dd87
SuhasNag12 <suhas.nag@perficient.com>
2025-12-21 19:56:05 +0530

    Private Constructor -> Demo of displaying error while compiling.

--- src/App.java
+++ src/App.java
@@ -2,27 +2,15 @@ class Person {
     String name;
     int age;
 
-    Person() {
-        System.out.println("This is a default constructor.");
-    }
-
-    Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-        System.out.println("This is a Parameterized Constructor,\nName: " + name + "\nAge: " + age);
-    }
-
-    Person(Person obj2) {
-        name = obj2.name;
-        age = obj2.age;
-        System.out.println("This is a Copy Constructor,\nName: " + name + "\nAge: " + age);
+    private Person() {
+        System.out.println("This is from Private Constructor");
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        Person person2 = new Person("Suhas Nag", 30);
-        Person person3 = new Person(person2);
+        Person person1 = new Person(); // This will give error, since Private Constructor is called from a different
+                                       // class
+
     }
 }
